542cab770719fdfd43668a2657804c018db70dbc
makocchi <makocchi@gmail.com>
2015-03-19 17:03:20 +0900

    change pip md5

--- config/software/python-pip.rb
+++ config/software/python-pip.rb
@@ -5,7 +5,7 @@ dependency "python"
 
 version "latest" do
   source url: "https://bootstrap.pypa.io/get-pip.py",
-         md5: "0ddcdb782d8d7d0e6dd4661eeba1f728"
+         md5: "add41078298d8111714c6b87636714f5"
 end
 
 build do
